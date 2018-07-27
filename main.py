4cf8f2675c35ae763b3a1f53f6738f139637a426
Arham Jain <7073362+ajusa@users.noreply.github.com>
2018-07-27 17:23:20 -0400

    leaving work commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def volume_module(msg, check, rest):
 	else: return False
 
 def time_module(msg, check, rest):
-	if "minute" in msg and int(re.search(r'\d+', msg).group()):
+	if "minute" in msg and re.findall(r'[0-9]+', msg) and int(re.findall(r'[0-9]+', msg)[0]):
 		if check: return {"out": None, "inp": "text"}
 		print('Will run action in ' + re.search(r'\d+', msg).group())
 		execute(rest)
@@ -37,16 +37,22 @@ def execute(msg):
 			test = module(" ".join(clean[j:i]), True, " ".join(clean[i:]))
 			if test:
 				#module(" ".join(clean[j:i]), False, " ".join(clean[i:]))
-				parsed.append({module: test, "msg": " ".join(clean[j:i]), "rest": " ".join(clean).replace(" ".join(clean[j:i]), "")})
+				parsed.append({"func": module, "module": test, "msg": " ".join(clean[j:i]), "rest": " ".join(clean).replace(" ".join(clean[j:i]), "")})
 				j = i
 	print(parsed)
-	#for key, value in parsed.items():
-	#	if
+	if(len(parsed) > 0):
+		for item in parsed:
+			if item["module"]["out"] == None:
+				print("yehs")
+				item["func"](item["msg"], False, item["rest"])
+				return
+		parsed[0]["func"](parsed[0]["msg"], False, parsed[0]["rest"])
+
 def possible(arr):
 	if len(arr) == 1 or len(arr) == 0: return True
 	for i in range(len(arr) - 1):
 		if arr[i]["out"] != arr[i+1]["inp"]: return False
 	return True
 #print(possible([{"out": "action", "inp": "text"}, {"out": None, "inp": "text"}])) #set volume to 50 in 5 minutes
-phrase = "change my laptop volume to the current weather in 10 minutes"
-execute(phrase)
+phrase = "in 10 minutes change my laptop volume to the current weather"
+execute(" ".join(phrase.split(" ")[::-1]))
